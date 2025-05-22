acb03363d86fae5c21320af6ef0fd3da759f95e1
sgndgdu <sgndgdu@gmail.com>
2025-05-22 14:02:44 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// public/firebase-config.js
+
+// Firebase config bilgilerin
+const firebaseConfig = {
+  apiKey: "AIzaSyDEN6KIYynqlEW-Q_InblxStaX860dmtW0",
+  authDomain: "kantar-crm.firebaseapp.com",
+  projectId: "kantar-crm",
+  storageBucket: "kantar-crm.firebasestorage.app",
+  messagingSenderId: "138676377984",
+  appId: "1:138676377984:web:cee36fd97343bc3cb10965"
+};
+
+// Firebase başlat
+firebase.initializeApp(firebaseConfig);
