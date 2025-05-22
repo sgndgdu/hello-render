d7f81e078e23c62528a9e6bdc9d4393a29f48883
sgndgdu <sgndgdu@gmail.com>
2025-05-22 14:11:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path'); // ← bu eklenecek
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
+
+// Bu satırla public klasörünü statik hale getiriyoruz
+app.use(express.static(path.join(__dirname, 'public'))); // ← BU ÇOK ÖNEMLİ
 
 app.get('/', (req, res) => {
   res.send('Merhaba Dünya!');
