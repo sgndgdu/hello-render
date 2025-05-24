3a8726e0fc271d3fa98b022de502bb31b11171bb
sgndgdu <sgndgdu@gmail.com>
2025-05-24 14:24:58 +0300

    Update app.jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,25 +1,22 @@
 // app.jsx
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/tailwind.css";
 import Header from "./components/Header.jsx";
-import TestSelector from "./components/TestSelector.jsx";
 import ZeroTestForm from "./components/ZeroTestForm.jsx";
 import CornerLoadTestForm from "./components/CornerLoadTestForm.jsx";
 import RepeatabilityTestForm from "./components/RepeatabilityTestForm.jsx";
 import PerformanceTestForm from "./components/PerformanceTestForm.jsx";
 
 const App = () => {
-  const [selectedTest, setSelectedTest] = useState("zero");
-
   return (
-    <div className="p-6 max-w-4xl mx-auto">
+    <div className="p-6 max-w-5xl mx-auto space-y-10">
       <Header />
-      <TestSelector selectedTest={selectedTest} setSelectedTest={setSelectedTest} />
-      {selectedTest === "zero" && <ZeroTestForm />}
-      {selectedTest === "corner" && <CornerLoadTestForm />}
-      {selectedTest === "repeat" && <RepeatabilityTestForm />}
-      {selectedTest === "performance" && <PerformanceTestForm />}
+
+      <ZeroTestForm />
+      <CornerLoadTestForm />
+      <RepeatabilityTestForm />
+      <PerformanceTestForm />
     </div>
   );
 };
